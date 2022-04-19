6cbd1c6c099b70d607b5e75c7f831b23abbd2425
Josh Duke <Nozzexx@gmail.com>
2022-04-19 17:06:41 -0500

    Commit 4.19.22
    Added Main Menu Functionality: New Game Branching (Board, Player Turn, Turn Count) Game Rules (Explains Game Rules, Return to Main Menu.) Exit (Exits Program)
    
    - Nozzexx

--- main.asm
+++ main.asm
@@ -3,32 +3,150 @@
 	introMessage: 	    .asciiz "Welcome to MIPS Connect 4! This game is meant for one player versus a computer.\n"
 	selectMessage:		.asciiz "\nPlease select an option from the menu below:"
 	selectionMenu:		.asciiz "\n  1. New Game\n  2. Game Rules\n  3. Exit Game"
+	inputCharacter:		.asciiz "\n\n>: "
+	gameRulesMessage:	.asciiz "\n     Hello and welcome to MIPS connect 4. Playing this game is quite simple. \n The objective is to fill four slots either horizontally, vertically, or diagonally. \n Your opponent is also trying to do the same and will work to try and interupt you. \n Simply choose a column to drop your token and the board will update to reflect \n your tokens location after it has 'fallen' down the column."
+	returnMessage:		.asciiz "\n\n Enter '0' to return to the main menu."
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	PlayerTurn:		.asciiz "\n PLAYERS TURN"
+	ComputerTurn:		.asciiz "\n COMPUTER TURN"
+	TurnLabel:		.asciiz "\n Turn Count: "
+	TurnCounter:		.word 0
+	
+
+	GameBoardRow1: 		.asciiz "\n\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow2:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow3:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow4:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow5:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow6:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |\n"
 .text
 
+MainMenu:
 # Display the intro message to the user. (Step 01)
 	li $v0, 4
 	la $a0, introMessage
 	syscall
 	
-# Display the intro message to the user. (Step 01)
+# Display the selection message to the user. (Step 02)
 	li $v0, 4
 	la $a0, selectMessage
 	syscall
 
-# Display the intro message to the user. (Step 01)
+# Display the divider line message to the user. (Step 03)
 	li $v0, 4
 	la $a0, dividerLine
 	syscall
 
-# Display the intro message to the user. (Step 01)
+# Display the selection message to the user. (Step 04)
 	li $v0, 4
 	la $a0, selectionMenu
 	syscall
 
-# Display the intro message to the user. (Step 01)
+# Display the divider message to the user. (Step 05)
 	li $v0, 4
 	la $a0, dividerLine
 	syscall
 	
+#Display the Input character to the user.
+	li $v0, 4
+	la $a0, inputCharacter
+	syscall
+	
+# Wait for the user menu input.
+	li $v0, 5
+	syscall
+	
+# Take Inut and Branch Based on choice made.
+
+	beq $v0, 1, NewGame
+
+	beq $v0, 2, GameRules
+	
+	beq $v0, 0, Exit
+NewGame:
+
+# Display the divider message to the user. (Step 05)
+	li $v0, 4
+	la $a0, dividerLine
+	syscall
+	
+	li $v0, 4
+	la $a0, PlayerTurn
+	syscall
+	
+	li $v0, 4
+	la $a0, TurnLabel
+	syscall
+	
+	li $v0, 4
+	la $a0, TurnCounter
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow1
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow2
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow3
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow4
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow5
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow6
+	syscall
+
+# Display the divider message to the user. (Step 05)
+	li $v0, 4
+	la $a0, dividerLine
+	syscall
+	
+#Display the Input character to the user.
+	li $v0, 4
+	la $a0, inputCharacter
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	
+GameRules:
+	li $v0, 4
+	la $a0, dividerLine
+	syscall
+	
+	li $v0, 4
+	la $a0, gameRulesMessage
+	syscall
+	
+	li $v0, 4
+	la $a0, dividerLine
+	syscall
+	
+	li $v0, 4
+	la $a0, returnMessage
+	syscall
+	
+	li $v0, 4
+	la $a0, inputCharacter
+	syscall
+
+	li $v0, 5
+	syscall
+	
+	beq $v0, 0, MainMenu
+InputError:
+Exit:
 	li $v0, 10 #exits application.
 	syscall
