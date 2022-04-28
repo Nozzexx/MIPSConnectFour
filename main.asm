1a245a75a6f0840c29bffa5fa0998d1c156884f4
Josh Duke <Nozzexx@gmail.com>
2022-04-28 13:09:12 -0500

    Commit 4.28.22
    Added Token String Replacement based on Turn. Updates to entire gameboard.

--- main.asm
+++ main.asm
@@ -14,16 +14,13 @@
 	TurnLabel:			.asciiz "\n Turn Count: "
 	TurnCounter:		.word 0
 	
-	PlayerToken:		.asciiz "X"
-	ComputerToken:		.asciiz "O"
-	
-	GameBoardRow0:		.asciiz "\n\n    1   2   3   4   5   6   7"
-	GameBoardRow1: 		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
-	GameBoardRow2:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
-	GameBoardRow3:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
-	GameBoardRow4:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow7:		.asciiz "\n\n    1   2   3   4   5   6   7"
+	GameBoardRow6: 		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
 	GameBoardRow5:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
-	GameBoardRow6:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |\n"
+	GameBoardRow4:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow3:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow2:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |"
+	GameBoardRow1:		.asciiz "\n  | _ | _ | _ | _ | _ | _ | _ |\n"
 	
 	InputColumn:		.asciiz "\nPlease select a Column:\n >:"
 	
@@ -48,6 +45,12 @@
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	PlayerArray:		.space  168
 	ComputerArray:		.space	168
+	
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
+
+	TooHighErrorMsg:	.asciiz "\n\n Error, Input too high. That column does not exist. Please enter a valid Input between 1-7.\n"
+	TooLowErrorMsg:		.asciiz "\n\n Error, Input too low. That column does not exist. Please enter a valid Input between 1-7.\n"
+	NoInputErrorMsg:	.asciiz "\n\n Error. No Input was detected, or that Column does not exist. Please enter a valid Input between 1-7.\n"
 	
 .text
 
@@ -118,23 +121,11 @@ ContinueTurn:
 	syscall
 	
 	li $v0, 4
-	la $a0, GameBoardRow0
+	la $a0, GameBoardRow7
 	syscall
 	
 	li $v0, 4
-	la $a0, GameBoardRow1
-	syscall
-	
-	li $v0, 4
-	la $a0, GameBoardRow2
-	syscall
-	
-	li $v0, 4
-	la $a0, GameBoardRow3
-	syscall
-	
-	li $v0, 4
-	la $a0, GameBoardRow4
+	la $a0, GameBoardRow6
 	syscall
 	
 	li $v0, 4
@@ -142,7 +133,19 @@ ContinueTurn:
 	syscall
 	
 	li $v0, 4
-	la $a0, GameBoardRow6
+	la $a0, GameBoardRow4
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow3
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow2
+	syscall
+	
+	li $v0, 4
+	la $a0, GameBoardRow1
 	syscall
 
 # Display the divider message to the user. (Step 05)
@@ -165,7 +168,10 @@ ContinueTurn:
 	beq $v0, 5, ColumnFiveIter
 	beq $v0, 6, ColumnSixIter
 	beq $v0, 7, ColumnSevenIter
-
+	
+	bgt $v0, 7, TooHighError
+	blt $v0, 1, TooLowError
+	
 ColumnOneIter:
 	addi $s0, $s0, 1
 	j AddToColumnOne
@@ -284,6 +290,7 @@ GetColumnSevenKey:
 UpdatePlayerArray:
 
 	sw $t6, PlayerArray($t0)
+	li $a1,'X'
 	beq $v0, 1, UpdateColumnOneString
 	beq $v0, 2, UpdateColumnTwoString
 	beq $v0, 3, UpdateColumnThreeString
@@ -295,6 +302,7 @@ UpdatePlayerArray:
 UpdateComputerArray:
 
 	sw $t6, ComputerArray($t0)
+	li $a1,'O'
 	beq $v0, 1, UpdateColumnOneString
 	beq $v0, 2, UpdateColumnTwoString
 	beq $v0, 3, UpdateColumnThreeString
@@ -305,18 +313,465 @@ UpdateComputerArray:
 	
 UpdateColumnOneString:
 
+	beq $s0, 1, UpdateRow1Col1
+	beq $s0, 2, UpdateRow2Col1
+	beq $s0, 3, UpdateRow3Col1
+	beq $s0, 4, UpdateRow4Col1
+	beq $s0, 5, UpdateRow5Col1
+	beq $s0, 6, UpdateRow6Col1
+
+UpdateRow1Col1:
+
+	li $t4, 0
+	la $t4, GameBoardRow1
+	lb $a0,5($t4)
+
+	sb $a1, 5($t4)
+
 	j CompleteTurn
+
+UpdateRow2Col1:
+
+	li $t4, 0
+	la $t4, GameBoardRow2
+	lb $a0,5($t4)
+
+	sb $a1, 5($t4)
+
+	j CompleteTurn
+	
+UpdateRow3Col1:
+
+	li $t4, 0
+	la $t4, GameBoardRow3
+	lb $a0,5($t4)
+
+	sb $a1, 5($t4)
+
+	j CompleteTurn
+UpdateRow4Col1:
+
+	li $t4, 0
+	la $t4, GameBoardRow4
+	lb $a0,5($t4)
+
+	sb $a1, 5($t4)
+
+	j CompleteTurn
+UpdateRow5Col1:
+
+	li $t4, 0
+	la $t4, GameBoardRow5
+	lb $a0,5($t4)
+
+	sb $a1, 5($t4)
+
+	j CompleteTurn
+UpdateRow6Col1:
+
+	li $t4, 0
+	la $t4, GameBoardRow6
+	lb $a0,5($t4)
+
+	sb $a1, 5($t4)
+
+	j CompleteTurn
+	
 UpdateColumnTwoString:
+
+	beq $s1, 1, UpdateRow1Col2
+	beq $s1, 2, UpdateRow2Col2
+	beq $s1, 3, UpdateRow3Col2
+	beq $s1, 4, UpdateRow4Col2
+	beq $s1, 5, UpdateRow5Col2
+	beq $s1, 6, UpdateRow6Col2
+
+UpdateRow1Col2:
+
+	li $t4, 0
+	la $t4, GameBoardRow1
+	lb $a0,9($t4)
+
+	sb $a1, 9($t4)
+
 	j CompleteTurn
+
+UpdateRow2Col2:
+
+	li $t4, 0
+	la $t4, GameBoardRow2
+	lb $a0,9($t4)
+
+	sb $a1, 9($t4)
+
+	j CompleteTurn
+	
+UpdateRow3Col2:
+
+	li $t4, 0
+	la $t4, GameBoardRow3
+	lb $a0,9($t4)
+
+	sb $a1, 9($t4)
+
+	j CompleteTurn
+UpdateRow4Col2:
+
+	li $t4, 0
+	la $t4, GameBoardRow4
+	lb $a0,9($t4)
+
+	sb $a1, 9($t4)
+
+	j CompleteTurn
+UpdateRow5Col2:
+
+	li $t4, 0
+	la $t4, GameBoardRow5
+	lb $a0,9($t4)
+
+	sb $a1, 9($t4)
+
+	j CompleteTurn
+UpdateRow6Col2:
+
+	li $t4, 0
+	la $t4, GameBoardRow6
+	lb $a0,9($t4)
+
+	sb $a1, 9($t4)
+
+	j CompleteTurn
+
+	
 UpdateColumnThreeString:
+
+	beq $s2, 1, UpdateRow1Col3
+	beq $s2, 2, UpdateRow2Col3
+	beq $s2, 3, UpdateRow3Col3
+	beq $s2, 4, UpdateRow4Col3
+	beq $s2, 5, UpdateRow5Col3
+	beq $s2, 6, UpdateRow6Col3
+
+UpdateRow1Col3:
+
+	li $t4, 0
+	la $t4, GameBoardRow1
+	lb $a0,13($t4)
+
+	sb $a1, 13($t4)
+
 	j CompleteTurn
+
+UpdateRow2Col3:
+
+	li $t4, 0
+	la $t4, GameBoardRow2
+	lb $a0,13($t4)
+
+	sb $a1, 13($t4)
+
+	j CompleteTurn
+	
+UpdateRow3Col3:
+
+	li $t4, 0
+	la $t4, GameBoardRow3
+	lb $a0,13($t4)
+
+	sb $a1, 13($t4)
+
+	j CompleteTurn
+UpdateRow4Col3:
+
+	li $t4, 0
+	la $t4, GameBoardRow4
+	lb $a0,13($t4)
+
+	sb $a1, 13($t4)
+
+	j CompleteTurn
+UpdateRow5Col3:
+
+	li $t4, 0
+	la $t4, GameBoardRow5
+	lb $a0,13($t4)
+
+	sb $a1, 13($t4)
+
+	j CompleteTurn
+UpdateRow6Col3:
+
+	li $t4, 0
+	la $t4, GameBoardRow6
+	lb $a0,13($t4)
+
+	sb $a1, 13($t4)
+
+	j CompleteTurn
+	
 UpdateColumnFourString:
+
+	beq $s3, 1, UpdateRow1Col4
+	beq $s3, 2, UpdateRow2Col4
+	beq $s3, 3, UpdateRow3Col4
+	beq $s3, 4, UpdateRow4Col4
+	beq $s3, 5, UpdateRow5Col4
+	beq $s3, 6, UpdateRow6Col4
+
+UpdateRow1Col4:
+
+	li $t4, 0
+	la $t4, GameBoardRow1
+	lb $a0,17($t4)
+
+	sb $a1, 17($t4)
+
 	j CompleteTurn
+
+UpdateRow2Col4:
+
+	li $t4, 0
+	la $t4, GameBoardRow2
+	lb $a0,17($t4)
+
+	sb $a1, 17($t4)
+
+	j CompleteTurn
+	
+UpdateRow3Col4:
+
+	li $t4, 0
+	la $t4, GameBoardRow3
+	lb $a0,17($t4)
+
+	sb $a1, 17($t4)
+
+	j CompleteTurn
+UpdateRow4Col4:
+
+	li $t4, 0
+	la $t4, GameBoardRow4
+	lb $a0,17($t4)
+
+	sb $a1, 17($t4)
+
+	j CompleteTurn
+UpdateRow5Col4:
+
+	li $t4, 0
+	la $t4, GameBoardRow5
+	lb $a0,17($t4)
+
+	sb $a1, 17($t4)
+
+	j CompleteTurn
+UpdateRow6Col4:
+
+	li $t4, 0
+	la $t4, GameBoardRow6
+	lb $a0,17($t4)
+
+	sb $a1, 17($t4)
+
+	j CompleteTurn
+	
 UpdateColumnFiveString:
+
+	beq $s4, 1, UpdateRow1Col5
+	beq $s4, 2, UpdateRow2Col5
+	beq $s4, 3, UpdateRow3Col5
+	beq $s4, 4, UpdateRow4Col5
+	beq $s4, 5, UpdateRow5Col5
+	beq $s4, 6, UpdateRow6Col5
+	
+UpdateRow1Col5:
+
+	li $t4, 0
+	la $t4, GameBoardRow1
+	lb $a0,21($t4)
+
+	sb $a1, 21($t4)
+
 	j CompleteTurn
+
+UpdateRow2Col5:
+
+	li $t4, 0
+	la $t4, GameBoardRow2
+	lb $a0,21($t4)
+
+	sb $a1, 21($t4)
+
+	j CompleteTurn
+	
+UpdateRow3Col5:
+
+	li $t4, 0
+	la $t4, GameBoardRow3
+	lb $a0,21($t4)
+
+	sb $a1, 21($t4)
+
+	j CompleteTurn
+UpdateRow4Col5:
+
+	li $t4, 0
+	la $t4, GameBoardRow4
+	lb $a0,21($t4)
+
+	sb $a1, 21($t4)
+
+	j CompleteTurn
+UpdateRow5Col5:
+
+	li $t4, 0
+	la $t4, GameBoardRow5
+	lb $a0,21($t4)
+
+	sb $a1, 21($t4)
+
+	j CompleteTurn
+UpdateRow6Col5:
+
+	li $t4, 0
+	la $t4, GameBoardRow6
+	lb $a0,21($t4)
+
+	sb $a1, 21($t4)
+
+	j CompleteTurn
+	
 UpdateColumnSixString:
+	
+	beq $s5, 1, UpdateRow1Col6
+	beq $s5, 2, UpdateRow2Col6
+	beq $s5, 3, UpdateRow3Col6
+	beq $s5, 4, UpdateRow4Col6
+	beq $s5, 5, UpdateRow5Col6
+	beq $s5, 6, UpdateRow6Col6
+
+UpdateRow1Col6:
+
+	li $t4, 0
+	la $t4, GameBoardRow1
+	lb $a0,25($t4)
+
+	sb $a1, 25($t4)
+
 	j CompleteTurn
-UpdateColumnSevenString:	
+
+UpdateRow2Col6:
+
+	li $t4, 0
+	la $t4, GameBoardRow2
+	lb $a0,25($t4)
+
+	sb $a1, 25($t4)
+
+	j CompleteTurn
+	
+UpdateRow3Col6:
+
+	li $t4, 0
+	la $t4, GameBoardRow3
+	lb $a0,25($t4)
+
+	sb $a1, 25($t4)
+
+	j CompleteTurn
+UpdateRow4Col6:
+
+	li $t4, 0
+	la $t4, GameBoardRow4
+	lb $a0,25($t4)
+
+	sb $a1, 25($t4)
+
+	j CompleteTurn
+UpdateRow5Col6:
+
+	li $t4, 0
+	la $t4, GameBoardRow5
+	lb $a0,25($t4)
+
+	sb $a1, 25($t4)
+
+	j CompleteTurn
+UpdateRow6Col6:
+
+	li $t4, 0
+	la $t4, GameBoardRow6
+	lb $a0,25($t4)
+
+	sb $a1, 25($t4)
+
+	j CompleteTurn
+	
+UpdateColumnSevenString:
+
+	beq $s6, 1, UpdateRow1Col7
+	beq $s6, 2, UpdateRow2Col7
+	beq $s6, 3, UpdateRow3Col7
+	beq $s6, 4, UpdateRow4Col7
+	beq $s6, 5, UpdateRow5Col7
+	beq $s6, 6, UpdateRow6Col7	
+
+UpdateRow1Col7:
+
+	li $t4, 0
+	la $t4, GameBoardRow1
+	lb $a0,29($t4)
+
+	sb $a1, 29($t4)
+
+	j CompleteTurn
+
+UpdateRow2Col7:
+
+	li $t4, 0
+	la $t4, GameBoardRow2
+	lb $a0,29($t4)
+
+	sb $a1, 29($t4)
+
+	j CompleteTurn
+	
+UpdateRow3Col7:
+
+	li $t4, 0
+	la $t4, GameBoardRow3
+	lb $a0,29($t4)
+
+	sb $a1, 29($t4)
+
+	j CompleteTurn
+UpdateRow4Col7:
+
+	li $t4, 0
+	la $t4, GameBoardRow4
+	lb $a0,29($t4)
+
+	sb $a1, 29($t4)
+
+	j CompleteTurn
+UpdateRow5Col7:
+
+	li $t4, 0
+	la $t4, GameBoardRow5
+	lb $a0,29($t4)
+
+	sb $a1, 29($t4)
+
+	j CompleteTurn
+UpdateRow6Col7:
+
+	li $t4, 0
+	la $t4, GameBoardRow6
+	lb $a0,29($t4)
+
+	sb $a1, 29($t4)
+
 	j CompleteTurn
 
 PTurn:
@@ -325,9 +780,6 @@ PTurn:
 	syscall
 	
 	addi $t1, $zero, 1
-	
-	#Load Player Token
-	lw $t5, PlayerToken
 	
 	j ContinueTurn
 CTurn:
@@ -364,6 +816,39 @@ GameRules:
 	
 	beq $v0, 0, MainMenu
 InputError:
+
+
+TooHighError:
+
+	add $t7, $t7, -1
+
+	li $v0, 4
+	la $a0, TooHighErrorMsg
+	syscall
+	
+	li $v0, 4
+	la $a0, dividerLine
+	syscall
+	
+	j ContinueTurn
+
+TooLowError:
+
+	add $t7, $t7, -1
+	
+	li $v0, 4
+	la $a0, dividerLine
+	syscall
+
+	li $v0, 4
+	la $a0, TooLowErrorMsg
+	syscall
+	
+	li $v0, 4
+	la $a0, dividerLine
+	syscall
+	
+	j ContinueTurn
 Exit:
 	li $v0, 10 #exits application.
 	syscall
